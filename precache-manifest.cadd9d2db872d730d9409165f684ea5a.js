1be3f801f8d0a50e8483cf8d8440f10ab1b25875
Manvi <manvigupta1987@gmail.com>
2020-10-01 03:59:26 -0400

    Updates

---
+++ precache-manifest.cadd9d2db872d730d9409165f684ea5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97cdd7c074b65283efe451f0bf99ba49",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "698154054a4957c39b73",
+    "url": "/monsters-rolodex/static/css/main.ac4e7427.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698154054a4957c39b73",
+    "url": "/monsters-rolodex/static/js/main.faf6c0a5.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
